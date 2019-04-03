514277175ed303bd684cab35d007ab203ca9f28e
Aaron <aacheng17@gmail.com>
2019-04-02 20:33:33 -0400

    Hopefully hotdogs will stay on the track now

--- src/Hotdog.java
+++ src/Hotdog.java
@@ -2,13 +2,28 @@
 public class Hotdog extends TrackObject {
 	int radius = 10;
 	boolean collision = false;
+	Vector prevVelocity = new Vector(); // this is for velocity stuff
 	
 	@Override
 	public void tick() {
 		super.tick();
+		if (velocity.length() > 0) prevVelocity = velocity;
 		if (location.distance2(TaxiGame.taxi.location) <= radius*radius && !collision) {
 			collision = true;
-			velocity = TaxiGame.taxi.velocity;
+			if (prevVelocity.length() != 0) {
+				Vector curDest = new Vector(location.x + prevVelocity.x, location.y + prevVelocity.y);
+				Vector oppDest = new Vector(location.x - prevVelocity.x, location.y - prevVelocity.y);
+				if (curDest.distance2(TaxiGame.taxi.location) > oppDest.distance2(TaxiGame.taxi.location)) {
+					velocity = prevVelocity;
+				}
+				else {
+					velocity = prevVelocity.scale(-1);
+				}
+			}
+			else {
+				velocity = TaxiGame.taxi.velocity;
+			}
+			velocity.setLength(TaxiGame.taxi.velocity.length());
 			TaxiGame.taxi.velocity = TaxiGame.taxi.velocity.scale(0.5);
 		}
 		if (!(location.distance2(TaxiGame.taxi.location) <= radius*radius)) {
